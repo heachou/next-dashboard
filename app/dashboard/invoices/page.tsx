aaaa81e95c47a764c6cccda57956c80a5ccaf398
heachou <12442399990@qq.com>
2024-01-06 16:10:00 +0800

    Update code: Added loading component, updated dependencies, and fixed bugs

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,6 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+const sleep3 = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export default async function Page() {
+  await sleep3(1000);  
+  return <p>invoices Page1</p>;
 }
