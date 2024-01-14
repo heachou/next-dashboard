4f95604f098271e11a5a6ca87f939b942741a94b
heachou <12442399990@qq.com>
2024-01-14 19:42:29 +0800

    Add new files and update dependencies

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,28 @@ import {z} from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { AuthError } from 'next-auth';
+import { signIn } from '@/auth';
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    const result = await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+
 
 const FormSchema = z.object({
   id: z.string(),
